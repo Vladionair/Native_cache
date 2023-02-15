3e2eb9eb250e0c991fc91cb12564578918bb8a7f
Vladionair <45149849+Vladionair@users.noreply.github.com>
2023-02-15 11:40:21 +0300

    open

--- Native_cache.py
+++ Native_cache.py
@@ -1,1 +1,130 @@
 gAAAAABfUePzut701YJ7Oq1xtqzT45ykbNsH9rGrsU8YIhYlXhEzRHPswrhmEC5X5bLUcQi2FFH0BsMkni89AQJs2Kk8iSScSr_jundYCw7j25OgN54EVyfaF495oikhig1TTfHn2NLVpMJpQN84jybql8ZU_xWtejSYddfybXODs89N0dZGEgMEodVXKnq0qVTHxCihgkml5ECSd29NUCKZZcwlZ7vrEyDVKDcuAyabOmGC7vbXMyPIlGJYel1ZGWIVH0UyIQIZnYw9mJpLv6reb3uvWoIjPCOGIQ-RQNSFMWOnNzO5xXKztL7q5DOyZrK1qYe0B2NmzlfI1_NfPCJihu-5pPLoCH9FfELiCN5RXO_zgfPB9uAQzbCxM33VSrQydir_pHHV13nlKwlk-ScvcbHTBQyrXp-z_3kt8UV9-BDa2D5S9_UYYfxTOv4HnXdUahUVv7rlGT8uB0S4h19kAuSCkit3ZF4fxsut-UZoma2TgBylRxgCnRcMjwPxGDE9nAKgZYOMAq3HLVtYSvzQt7DXW7PQot13XH-buMOq9mUjlEu9zr2os3hn02G_hZeUvZjhJ8hEvHLLc-UqcP9KxDiM4niQ2XJIkrSZ9sj2u5Ca-6TaKnwmogfiVRXOixKYSOodRIajFDuvZw-y5KFPJreHCewtxqKemYWqIPebn0Xm_YH0ZQZV90f8jA2MNrIRwdJV0EHZKkYW85Nu0TsbLxvMIqOQCbUJ7ntXpBcoshhpthUnZWNrBCY-kVOeFJ1lGSMF0px9VHNIVCLfLPLEGiXBmgtml_LbE-Cjt21uHhI6XQ4IUBpvLynnpVWWKC_67sCnneLm-mAVwbD3puhR1WPE3krmJ5oc8MCNG2s9-XFzuVr8eX5Fhht5EjncR16D-hQ46kaWBktgUBjukao7iaJS1MpTEPE-EHmpUWtWzhBEUCIelE5iYuosXALrzNCru_WcHYXKRkoMXJ0cDKb4ZrsDN5a0tnA93k8AocU5OT_0eoG0OqA7lXxX8r0cT_hAvFGp_4Izlm3aqZ0lJiAUKfSIlZbABPBN5TDwP9PH2TvrmwBC1488QNMICEfolbFgWWJzRuQJSjJQ0OJhxyJvlyqYf6EEFd7Mman9MVF4S5sFf4xwpuHrMaPgRWoicVvLgeRF8BIZOyBgGw6XpnfpXjVucgvuKz0CIDlgEc9Ko4efhs9HY6qUtZlvNXuN-IBjnj2K5RC1pHMSt5B612xGWRBEv2sEmTLsKJQl3NwW4e8X4Q9mnG2JFfqGxT53YemDaEnyNdm1URBOMiBzpF81nq7RB9xZFgjuMe_O-qw0Ih0sudODeThPZIyMp7eOBnu5ly8GiugJlh7Jsr3FmDCH48VXf2dGlzJnNAhhNcK3eszbUcQx42rlAlwcP7tMAuR53f6gZ_o7W7LKOLxA-Xq4khFK-5_KBk43VCnuOJ3bD3SbFGc2UtovNEqB-UKdvWQunTtqyufgeYKpKhEEp0yVeh2IC9Ezfvvx-ARhcoi1x7zKgTfgqXusV8tySI2UE2l5sQOh0yoNUHuirYWv-5OSZRGw4orxeceLli3WM9IARINHMhHl3He5r-bodBa4B-y70cRH29pFiHa4aaEDbBu9scNe9YadC5a1blU7LKqcEiy87p2K9dyaCEyXdK1AA3rHzbPr1QjGTFDip0TguogG4iQ9oGBjqgzTClDG-XOLC0vQkypBRsR0UXM16YKs-YI7iCshnusQ6vTRY5aaODduoIt8_AgTcuvLIKMtmzH2yWRwToqUnwHrwqhBegsTGZaXwIt6OU3e-OGcIK6CQvec6qR742FSWKQnSsWEU4bO-HX7CtVEGHXE0vqwm1gftiKrVEOmqs7Zo37aIYjoWWb39cMr0SLGenSaGq51wCpcePV9ITlbsrmt3Dro3BjE_ylaR55mHd83yWCgE5SNvOjvOBOWSUPyCoPKWLsECMSUJUh43K2b4aGM-LFtcbp_H1sURP3hSflKUDnyh10ZCFDahXSyBsS5nM75pIqh9ydn2t56eDnNoqfggDb3V-Z34qMi41gon92tl6FQLzldOjqcuYP8eXoS4kc1BXnj4TZrFSoFoyYfiylmYtShbBNj3ExA5z63JaV8M1avwaSGaynW6PVewFskd_MP6ZmVrc7uPzkT9hn2Xho-VPx0OlyGCnmZ_u1mJir66gNOfyxFu3vkBUl6fvjsdQmCYOs0FrhcVTrqbdKWfdIZ6Z-jjh6vD1utlPKqjoJxybQwiWp6mnmYAMHIUac7R7gEagLfbv_mPneJuM30s_L44EsR48uxP6AWAmbhZdmHOCn38ltkcnvdBe5iuW7mnjrznz9mDHTb6ZCIiLmzG3aZRiYc9ZKhkKIUm0LiisZASFg9Q_mB7Mk7J2wHeKlSm11z1Ckc0KuAslqJfFliJFcRs2px045N8tfdHGdl7yP9f7nTZ3n7UrCMrrkhEd4aiVZLIdjt4rkTJEfF3SDO7rs09nJQiX2wx7HArVy2FB8B7EK31lax3-x-NmrvAhEfnfd2Kwh8QBKhEsilFzx1hhSNZnD6AQgvSmA3PYkTHcphZoaOI4POSFnohH-BxFpi3gvNn4XUgwk9Y7qFspdxfPxQoeyatzcNT6KHpc3XeVHuPP4SI58ImTR8vqHKPMx3KUyb8VINS9OJP4AScM2jJ7_Xd3D8kqMxfES7kSYSC0lBlGU97z9y9DPWLPTIkhaBTMjiP-qU5J198oiwzpBALtJcdiI9m49fh8mLjtapGejNDo8y8HuyHItcCB98hRBHhGXZRTzen4VegOwwpY3vfisPVOAsGK_Bdo9TeAxNVUsIPZHT5dp01rO_qp1ZlmjF4RAVBkUxDmOfmXdQq9HNVDBnVfASQtudA58MYJlDhnNQc6m8SK9sRZCvBy8uZ3xgNAMzo0Igav6qxN3QLg6xyJz8979iXofIC7j6pmADzPkdBFP2BbWSxwc10YL78CjeZfxsc1ZBmC4_x4gagiSt6-D5ELgkO0ZjFA2nw9Dbh4nlGPm6GqeJG_15GWIIOyebWn6eJB0Mk29pglzfC_iiAa_V2dq5sBnIwif3O0Ys90DddjbusGTe9PBtIf5Qc7A6bII1UpGP40a8Q8DalQiK8dbc4wBbJhla_DsveYAEoOloonerlIrYJY8HRZls9O_NqWoxQ6mQ0EKtz5i4lliIkKLA58A2XkvZ_DMIVszLR_-WhKUzfoXp_FBh0dLkDTlu-wE7pqU9cJd1IR1050HwK2oX0sc0q6C_V3zXA8pROLj7bT9moftTQG7QY2nvV9Xb1OW5xkIY7LtA9wJouKOZCvkc_2RclG84KmHkzHJPc4dWR8sWLSXkIdrVzo1h0COzjkUbDf0y3Vz3ZU17Aqwi-EWzOMfKomp5kRoZMTLn9LjgJeedDCE8LTCPIRNZncqZdSI1a9KYZ9_FwGxLjyn2QaOlcuuRObht_-s4JtHJYH8BMA23LIMThRBIKRdjG5UOOXyEaGczSKlCTfgJm6QOKbL7_W7S4Cv-giHEGi5okuf8UMAfWbS7Yb1c9BWxkyIrTG6rjAkjsuk0s9Kg2VKMWaP0tWkv31ln56ymGX6QW47KjIDkOcMG4CKkoCHSynnNdH0YHTcKIDY2OOgu6DnKaDEmlge8nRFxVvivJOe4gkRH6awdZYvQIuZOFJGDYMZrSzWuegOzU696QmA-vjxTufuwGjn8_IVB2KuVXdCYxseLNxW8UNK0m73I0-iroj3DQhMlbIDt0tAwKtnPP6Lgc7zKL_XRKyhpeG3fKs9NKig-92S7vdHnoIO7FIlT23GjdfUJpQ4jtGlqRvFj_7n0vtGXo9VW18jG8sNkkQS41E23gLcSD2HnpIDZomXjekmGIyZJXVBOlXb9aR-jy5K1VMfBpEzZdPY_xbHV3Q7WQmBuNUW6BW6ypZnT7YLaGFGpz0BuBgqDPCESRKSii8VzZ6KDM3urvXaXOnd9E0jjC3zujQZtM_Xu3vSy_o_QWQOYW5mnz7OnW-Vr6JVpUglbW-aGYjVYqyo68tUmEONezSOtk6Z4lUUfrfwndj7KINuyZWV9EG52Nkqat_BkKhk2qlb0ekFdqLTiJ_CJFkqBCgBgrmEg-jOBGI-VaJb55IjidiD7P5cPEvVrUdVxENTKtMZUcxTHfeW81OJgp9ykf6BYMZKtZ3RUNc9dEYT4a8PJGRIingJWFX5Bs9QVYcgdx4ovLzuoGAf1Fi3EoGx60Qc8uRv_PnpMxnpdw9HZq72N7L4HPEo3bCZvJ5CELYD2OqU2i7prHdGpRbh3jERgmQohPjd2CJ69HjGnQLsRxiA0a6yHpcohBNnKQ7dFmKdoCwDcsad_fwfi9DMtLl_7ve3rhlSAww1-K2S3ur8hE3RsZBl3A7zdIdFrrbZ1pCJmagCAGDXeKTNMe-trRRZeoPN6P7PBKJvfnIkZoyGuNTzP0puDV8s6RpY8xr6PDfkpdqgbtjNOQmIZRKoR8SXtQJSb
+
+class NativeCache:
+
+    def __init__(self, sz):
+
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+        self.hits = [0] * self.size
+        self.storage = {}
+
+    def hash_fun(self, value):
+
+        return hash(str(value))
+
+    def put(self, value):
+
+        self.storage[self.hash_fun(value)] = value
+
+    def find(self, key):
+
+        if key in self.slots or key in self.storage.keys():
+            return True
+        else:
+            return False
+
+    def get(self, key):
+
+        if key in self.slots:
+            i = self.slots.index(key)
+            self.hits[i] += 1
+            return self.values[i]
+        else:
+            if key in self.storage.keys():
+                i = self.hits.index(min(self.hits))
+                if self.slots[i] is not None:
+                    self.storage[self.slots[i]] = self.values[i]
+                else:
+                    pass
+                self.slots[i] = key
+                self.values[i] = self.storage[key]
+                self.hits[i] = 1
+                del self.storage[key]
+                return self.values[i]
+            else:
+                return None
+
+    def delete(self, key):
+
+        if key in self.slots:
+            i = self.slots.index(key)
+            self.slots[i] = None
+            self.values[i] = None
+            self.hits[i] = 0
+            return True
+        else:
+            if key in self.storage.keys():
+                del self.storage[key]
+                return True
+            else:
+                return False
+
+import random
+import unittest
+
+class Test_NativeCache(unittest.TestCase):
+
+    def test_hash_fun(self):
+
+        for i in range(10):
+            self.assertEqual(test.hash_fun('1' * i), test.hash_fun('1' * i))
+
+    def test_put(self):
+
+        for i in range(10):
+            test.put(i)
+            self.assertIn(test.hash_fun(i), test.storage.keys())
+            self.assertIn(i, test.storage.values())
+
+    def test_find(self):
+
+        for i in range(10):
+            test.put(i)
+        for i in range(10):
+            test.get(random.choice(list(test.storage.keys())))
+            self.assertEqual(test.find(test.hash_fun(i)), True)
+
+    def test_get(self):
+
+        for i in range(10):
+            test.put(i)
+            self.assertEqual(test.get(test.hash_fun(i)), i)
+
+    def test_delete(self):
+
+        for i in range(10):
+            test.put(i)
+            self.assertIn(test.hash_fun(i), test.storage.keys())
+        for i in range(10):
+            test.delete(test.hash_fun(i))
+            self.assertNotIn(test.hash_fun(i), test.storage.keys())
+
+    def test_displace(self):
+
+        for i in range(10):
+            test.put('a' * (i + 1))
+        for i in range(10):
+            test.get(random.choice(list(test.storage.keys())))
+        test.put('b')
+        for i in range(10):
+            test.get(test.hash_fun('b'))
+        self.assertIn(test.hash_fun('b'), test.slots)
+        self.assertIn('b', test.values)
+
+    def test_hits(self):
+
+        test.put('b')
+        for i in range(10):
+            test.get(test.hash_fun('b'))
+            self.assertEqual(test.hits[0], i + 1)
+
+    def tearDown(self):
+
+        test.__init__(10)
+
+if __name__ == '__main__':
+    
+    test = NativeCache(10)
+    unittest.main(verbosity=2)
